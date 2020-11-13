4bf3b190f08a0e8208d51408415926f45348737c
Dmitriy <45269664+drdolgosheev@users.noreply.github.com>
2020-11-13 14:58:12 +0300

    Add files via upload

---
+++ Johnson's algorithm/cmake-build-debug/CMakeFiles/Johnson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Johnson.dir/main.cpp.obj"
+  "Johnson.pdb"
+  "Johnson.exe"
+  "Johnson.exe.manifest"
+  "libJohnson.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Johnson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
